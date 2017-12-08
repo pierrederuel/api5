fffbc24f70de7a468659b242ce0fef540954d4d5
Pierre Deruel <pierre.deruel@gmail.com>
2017-12-08 08:49:44 +0100

    Modification méthode d'envoi de notif

--- src/main/java/fr/enelia/dashboardapi/services/impl/NotificationServiceImpl.java
+++ src/main/java/fr/enelia/dashboardapi/services/impl/NotificationServiceImpl.java
@@ -6,6 +6,7 @@ import de.bytefish.fcmjava.requests.data.DataUnicastMessage;
 import de.bytefish.fcmjava.requests.notification.NotificationPayload;
 import de.bytefish.fcmjava.responses.FcmMessageResponse;
 import de.bytefish.fcmjava.responses.FcmMessageResultItem;
+import fr.enelia.dashboardapi.entities.Token;
 import fr.enelia.dashboardapi.entities.Utilisateur;
 import fr.enelia.dashboardapi.helpers.FirebaseResponse;
 import fr.enelia.dashboardapi.helpers.HeaderRequestInterceptor;
@@ -19,10 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -44,45 +42,49 @@ public class NotificationServiceImpl implements NotificationService {
         restTemplate.setInterceptors(interceptors);
 
         for (Utilisateur utilisateur:utilisateurs) {
-            if (utilisateur.getToken() != null && utilisateur.getToken().length() > 0) {
-                LOGGER.info("Envoi notification à " + utilisateur.getUsername());
+            Iterator<Token> itTokens = utilisateur.getTokens().iterator();
+            while (itTokens.hasNext()) {
+                Token token = itTokens.next();
+                if (token.getFcmToken() != null && token.getFcmToken().length() > 0) {
+                    LOGGER.info("Envoi notification à " + utilisateur.getUsername());
 
-                JSONObject body = new JSONObject();
-                // JsonArray registration_ids = new JsonArray();
-                // body.put("registration_ids", registration_ids);
-                body.put("to", utilisateur.getToken());
-                body.put("priority", "high");
-                // body.put("dry_run", true);
+                    JSONObject body = new JSONObject();
+                    // JsonArray registration_ids = new JsonArray();
+                    // body.put("registration_ids", registration_ids);
+                    body.put("to", token.getFcmToken());
+                    body.put("priority", "high");
+                    // body.put("dry_run", true);
 
-                JSONObject notification = new JSONObject();
-                notification.put("body", notifBody);
-                notification.put("title", title);
-                notification.put("sound", "default");
-                // notification.put("icon", "myicon");
-
-
-                body.put("notification", notification);
-                body.put("data", data);
-
-                HttpEntity<String> entity = new HttpEntity<>(body.toString());
+                    JSONObject notification = new JSONObject();
+                    notification.put("body", notifBody);
+                    notification.put("title", title);
+                    notification.put("sound", "default");
+                    // notification.put("icon", "myicon");
 
 
-                FirebaseResponse firebaseResponse = restTemplate.postForObject("https://fcm.googleapis.com/fcm/send", entity, FirebaseResponse.class);
-                CompletableFuture<FirebaseResponse> pushNotification = CompletableFuture.completedFuture(firebaseResponse);
-                CompletableFuture.allOf(pushNotification).join();
+                    body.put("notification", notification);
+                    body.put("data", data);
 
-                try {
-                    FirebaseResponse response = pushNotification.get();
-                    if (response.getSuccess() == 1) {
-                        LOGGER.info("push notification sent ok!");
-                    } else {
-                        LOGGER.error("error sending push notifications: " + firebaseResponse.toString());
+                    HttpEntity<String> entity = new HttpEntity<>(body.toString());
+
+
+                    FirebaseResponse firebaseResponse = restTemplate.postForObject("https://fcm.googleapis.com/fcm/send", entity, FirebaseResponse.class);
+                    CompletableFuture<FirebaseResponse> pushNotification = CompletableFuture.completedFuture(firebaseResponse);
+                    CompletableFuture.allOf(pushNotification).join();
+
+                    try {
+                        FirebaseResponse response = pushNotification.get();
+                        if (response.getSuccess() == 1) {
+                            LOGGER.info("push notification sent ok!");
+                        } else {
+                            LOGGER.error("error sending push notifications: " + firebaseResponse.toString());
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
                     }
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
                 }
             }
         }
